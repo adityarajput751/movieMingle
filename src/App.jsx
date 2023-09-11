dee1a311edf7c1c493ca3e6eaa06caf8fc7e2243
Aditya Rajput <your.email@example.comadityarajput751@gmail.com>
2023-09-12 03:49:37 +0530

    done changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,48 @@ import { useState, useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 import { getApiConfiguration } from "./store/reducers/homeSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Details from "./pages/details/Details";
+import Explore from "./pages/explore/Explore";
+import SearchResult from "./pages/searchResult/SearchResult";
+import PageNotFound from "./pages/404/PageNotFound";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  const dispatch = useDispatch()
-  const {url} = useSelector(state => state.home)
-  console.log(url,'fkefknv');
+  const dispatch = useDispatch();
+  const { url } = useSelector((state) => state.home);
+  console.log(url, "fkefknv");
   useEffect(() => {
-    apiTesting();
+    fetchApiConfig();
   }, []);
 
-  const apiTesting = () => {
-    fetchDataFromApi("/movie/popular").then((res) => {
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
       console.log(res);
-      dispatch(getApiConfiguration(res))
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original"
+      }
+      dispatch(getApiConfiguration(url));
     });
   };
 
-  
-  return <div className="App">App</div>;
+  return (
+    <BrowserRouter>
+      {/* <Header /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer /> */}
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import "./style.scss";
+import { useNavigate } from "react-router-dom";
+import useFetch from "../../../hooks/useFetch";
+import { useSelector } from "react-redux";
 
 const HeroBanner = () => {
-  return (
-    <div>HeroBanner</div>
-  )
-}
+  const [background, setBackground] = useState("");
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+  const { url } = useSelector((state) => state.home);
+  const { data, loading } = useFetch("/movie/upcoming");
+console.log(background,'rajput');
+  useEffect(() => {
+    const bg =
+      url?.backdrop +
+      data?.results[Math?.floor(Math?.random() * 20)]?.backdrop_path;
+    setBackground(bg);
+  }, [data]);
 
-export default HeroBanner
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+    }
+  };
+  return (
+    <div className="heroBanner">
+      <div className="wrapper">
+        <div className="heroBannerContent">
+          <span className="title">Welcome.</span>
+          <span className="subTitle">
+            Millions of movies, TV shows and people to discover. Explore now.
+          </span>
+          <div className="searchInput">
+            <input
+              type="text"
+              className="text"
+              placeholder="Search for a movie or TV show...."
+              onKeyUp={searchQueryHandler}
+              onChange={(e) => setQuery(e.target.value)}
+            />
+            <button>Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroBanner;
